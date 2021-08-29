4364538c8b1bf6e043e6c624707217d5e79b8e45
Crowdbotics <team@crowdbotics.com>
2021-08-29 09:49:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "yellow_cell_30176Identifier",
+  url: "https://yellow_cell_30176Identifier.botics.co",
+  api: "https://yellow_cell_30176Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
